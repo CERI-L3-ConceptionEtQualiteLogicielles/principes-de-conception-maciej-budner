3582ef7b581383262806e5827a90edf08dffc436
Maciej <maciej.budner@alumni.univ-avignon.fr>
2025-02-13 10:04:29 +0100

    Je voulais tester l'affichage pour voir si j'ai pas fait une erreur

--- src/test/java/ContactTeste.java
+++ src/test/java/ContactTeste.java
@@ -1,4 +1,20 @@
+
+import exo1.Contact;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContactTeste {
+
+    @Test
+    public void testAfficheContact(){
+
+        Contact contact;
+        contact = new Contact("test","06");
+        contact.afficheContacts();
+
+    }
 
 
 }
